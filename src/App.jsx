f08308c8090287d69ed47ddff3b05b28e8a422d4
Temuri Tsutskiridze <temuri.tsutskiridze.04@gmail.com>
2023-09-23 19:58:04 +0400

    add strength functionality and custom checkox, render divs with map

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header  from './components/header/header'
-import GeneratorPart from './components/GeneratorPart/password-generator-part'
-import Container from './components/Container/container'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./components/header/header";
+import GeneratorPart from "./components/GeneratorPart/password-generator-part";
+import Container from "./components/Container/container";
 
- const App =  () => {
-
- 
+const App = () => {
   return (
-  
-    <div>
-     <Header/>
-     <GeneratorPart/>
-     <Container/>
-    </div>
-
-  )
-  }
-export default App
+    <>
+      <Header />
+      <GeneratorPart />
+      <Container />
+    </>
+  );
+};
+export default App;
--- src/components/Container/container.jsx
+++ src/components/Container/container.jsx
@@ -1,73 +1,89 @@
-import './container.css'
-import arrowImg from "../../assets/icon-arrow-right.svg"
-import { useState } from 'react'
+import "./container.css";
+import arrowImg from "../../assets/icon-arrow-right.svg";
+import { useEffect, useState } from "react";
 
-
-
+const data = [
+  "Include Uppercase Letters",
+  "Include Lowercase Letters",
+  "Include Numbers",
+  "Include Symbols",
+];
 
 const Container = () => {
-const [rangeValue, setRangeValue] = useState (10)
-const rangeHandler = (e) => {
-   setRangeValue(e.target.value)
-}
-const changeBackgroundColor = () =>{
-  setBackgroundColor('#A4FFAF')
-}
+  const [rangeValue, setRangeValue] = useState(10);
+  const [strength, setStrength] = useState(0);
 
-const uppercaseHandler = () => {
+  const rangeHandler = (e) => {
+    setRangeValue(e.target.value);
+  };
 
-}
+  function handleChange(e) {
+    if (e.target.checked) {
+      setStrength((prev) => prev + 1);
+    } else {
+      setStrength((prev) => prev - 1);
+    }
+  }
 
-const LowercaseHandle = () => {
+  useEffect(() => {
+    console.log(strength);
+  }, [strength]);
 
-}
-const numbersHandle = () => {
-
-}
-const symbolsHandle = () => {
-
-}
-    return ( 
-       <div className='main'> 
-        <div className='text-number'>
-            <h2 className='character-length'>Character Length</h2>
-            <h1 className='numbers'>{rangeValue}</h1>
-        </div>
-        <input className="range" type='range' max={20} onChange={rangeHandler} value={rangeValue} />
-        <div className='boxes'>
-          <div className='first'>
-            <input type="checkbox" onClick={changeBackgroundColor} className="first-box"/>
-            <h2 className='text'>Include Uppercase Letters</h2>
-          </div>
-          <div className='second'>
-          <input type="checkbox"  className="second-box" />
-            <h2 className='text'>Include Lowercase Letters</h2>
-          </div>
-          <div className='third'>
-          <input type="checkbox"  className="third-box" />
-            <h2 className='text'>Include Numbers</h2>
-          </div>
-          <div className='fourth'>
-          <input type="checkbox"  className="fourth-box" />
-            <h2 className='text'>Include Symbols</h2>
-          </div>
-        </div>
-        <div className="dark-box">
-            <h2 className='strength'>STRENGTH</h2>
-            <div className="text-and-scale">
-                <h2 className='scale'></h2>
-                <div className="colored-boxes a" ></div>
-                <div className="colored-boxes b" ></div>
-                <div className="colored-boxes c"></div>
-                <div className="colored-boxes d"></div>
+  return (
+    <div className="main">
+      <div className="text-number">
+        <h2 className="character-length">Character Length</h2>
+        <h1 className="numbers">{rangeValue}</h1>
+      </div>
+      <input
+        className="range"
+        type="range"
+        max={20}
+        onChange={rangeHandler}
+        value={rangeValue}
+      />
+      <div className="boxes">
+        {data.map((item, index) => {
+          return (
+            <div className="container-box" key={index}>
+              <input
+                type="checkbox"
+                className="checkbox"
+                onChange={handleChange}
+              />
+              <div className="custom-checkbox"></div>
+              <h2 className="text">{item}</h2>
             </div>
+          );
+        })}
+      </div>
+      <div className="dark-box">
+        <h2 className="strength">STRENGTH</h2>
+        <div className="text-and-scale">
+          <h2 className="scale"></h2>
+          <div
+            className={`colored-boxes ${strength === 1 ? "a" : undefined}`}
+          ></div>
+          <div className="colored-boxes b"></div>
+          <div className="colored-boxes c"></div>
+          <div className="colored-boxes d"></div>
         </div>
-        <button className="generate-part">
-            GENERATE
-            <svg className='icon-arrow' width="12" height="12" xmlns="http://www.w3.org/2000/svg"><path fill="#24232C" d="m5.106 12 6-6-6-6-1.265 1.265 3.841 3.84H.001v1.79h7.681l-3.841 3.84z"/></svg>
-        </button>
-       </div>
-        
-  )
-}
-export default Container
+      </div>
+      <button className="generate-part">
+        GENERATE
+        <svg
+          className="icon-arrow"
+          width="12"
+          height="12"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill="#24232C"
+            d="m5.106 12 6-6-6-6-1.265 1.265 3.841 3.84H.001v1.79h7.681l-3.841 3.84z"
+          />
+        </svg>
+      </button>
+    </div>
+  );
+};
+export default Container;
